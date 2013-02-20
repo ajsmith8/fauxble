4d0e21ad927c496fe581fd5e5ccb235918b40109
jrlapayne <jrlapayne@gmail.com>
2013-02-19 22:09:12 -0800

    achievables popdowns etc heroku

---
+++ db/seeds.rb
@@ -0,0 +1,47 @@
+Achievable.create(
+  title: "Learning Lover",
+  description: "You love to learn! Look at all those sexy facts!",
+  name: "facts learned",
+  image: "newton",
+  count: 4
+)
+
+Achievable.create(
+  title: "Shot Down",
+  description: "Your friends are killing you... Seriously.",
+  name: "losses streak",
+  image: "tupac",
+  count: 3 
+)
+
+Achievable.create(
+  title: "You Dislike Facts",
+  description: "Do you even know what a fact is?",
+  name: "losses",
+  image: "foxnews",
+  count: 2 
+)
+
+Achievable.create(
+  title: "Winning",
+  description: "Enough said.",
+  name: "wins",
+  image: "sheen",
+  count: 2
+)
+
+Achievable.create(
+  title: "Unfair advantage?",
+  description: "You\'re winning too many games. You\'re either God or cheating.",
+  name: "wins",
+  image: "armstrong",
+  count: 10
+)
+
+Achievable.create(
+  title: "Weapons of Mass Destruction",
+  description: "You\'re a weapon of mass destruction, smokin\' everyone!",
+  name: "wins streak",
+  image: "gwb",
+  count: 5
+)
