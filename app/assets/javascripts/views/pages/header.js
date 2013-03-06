4f7aac827c5fe4211e0d57ff813d138fdf816434
jrlapayne <jrlapayne@gmail.com>
2013-03-05 19:06:05 -0800

    profile in header

--- app/assets/javascripts/views/pages/header.js
+++ app/assets/javascripts/views/pages/header.js
@@ -7,14 +7,29 @@ Fauxble.Views.PagesHeader = Backbone.View.extend({
 	},
 	
 	initialize: function(options) {
-		
+		this.attr = options.attr;
+		this.user = this.attr.users.get(this.attr.current_user.get('id'));
 	},
 	
 	render: function() {
-		$(this.el).html(this.template({
-			
-		}));
+		var self = this;
+		
+		$(this.el).html(this.template());
+		
+		setTimeout(function() {
+			self.renderProfile();
+		}, 0);
+		
 		return this;
+	},
+	
+	renderProfile: function() {
+		var view = new Fauxble.Views.UsersShow({
+			attr: this.attr,
+			user: this.user,
+			is_sidebar: true
+		});
+		$(this.el).find('#profile').html(view.render().el);
 	}
 });
 //header
