716a4d49b5914a3cc8fad221f99d5051c55ecbbd
jrlapayne <jrlapayne@gmail.com>
2013-03-08 19:32:11 -0800

    unfixed the fb like fix

--- app/assets/javascripts/views/pages/aux.js
+++ app/assets/javascripts/views/pages/aux.js
@@ -114,7 +114,7 @@ Fauxble.Views.PagesAux = Backbone.View.extend({
 	
 	likeButton: function() {
 		$(this.el).find('#bottom').html(JST['pages/like']);
-		FB.XFBML.parse($(this.el).find('#bottom').find('.fb-like'));
+		FB.init({appId: '282324831851627', channelUrl: 'http://localhost:3000', status: true, xfbml: true});
 	}
 });
 
