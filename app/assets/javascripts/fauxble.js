5f38065c4118b995c976b0094902735089538075
jrlapayne <jrlapayne@gmail.com>
2013-03-29 17:33:29 -0700

    ie gradient fix merge

--- app/assets/javascripts/fauxble.js
+++ app/assets/javascripts/fauxble.js
@@ -62,11 +62,11 @@ window.Fauxble = {
 };
 
 function gaCustomVar(index, name, value, scope) {
-	_gaq.push(['_setCustomVar', index, name, value, scope]);
+	//_gaq.push(['_setCustomVar', index, name, value, scope]);
 }
 
 function gaEvent(category, action, label, value) {
-	_gaq.push(['_trackEvent', category, action, label, value]);
+	//_gaq.push(['_trackEvent', category, action, label, value]);
 }
 
 function gaPageview(url, user) {
@@ -79,11 +79,11 @@ function gaPageview(url, user) {
 	
 	gaEvent('Page View', String(id), url, parseInt(time) - 1360000000);
 	
-	_gaq.push(['_trackPageview', "/" + url]);
+	//_gaq.push(['_trackPageview', "/" + url]);
 }
 
 function fbLogin() {
-	//window.location = "http://localhost:3000/auth/facebook";
-	window.location = "http://salty-lowlands-9089.herokuapp.com/auth/facebook";
+	window.location = "http://localhost:3000/auth/facebook";
+	//window.location = "http://salty-lowlands-9089.herokuapp.com/auth/facebook";
 	//window.location = "http://fusegap.org/auth/facebook";
 }
